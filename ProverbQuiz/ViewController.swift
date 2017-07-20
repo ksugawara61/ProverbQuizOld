455e56bf8a41e465552a3f15ed2cb223f4269198
ksugawara61 <katsuya@kokohadoko.net>
2017-07-20 14:30:48 +0900

    Initial Commit

---
+++ ProverbQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProverbQuiz
+//
+//  Created by 菅原勝也 on 2017/07/20.
+//  Copyright © 2017年 nttr.inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
