063a6713c872b385f69b8f7a3f5622f535588f6c
ksugawara61 <katsuya@kokohadoko.net>
2017-07-26 00:26:37 +0900

    格言の追加・修正

--- ProverbQuiz/ViewController.swift
+++ ProverbQuiz/ViewController.swift
@@ -17,9 +17,17 @@ class ViewController: UIViewController {
                    ["name": "始皇帝", "image": "shikoutei", "proverb": "天下が戦に苦しむのは、諸侯があるからだ。"],
                    ["name": "徳川家康", "image": "ieyasu", "proverb": "戦いでは強い者が勝つ。辛抱の強い者が。"],
                    ["name": "諸葛亮", "image": "koumei", "proverb": "無欲でなければ志は立たず、穏やかでなければ道は遠い。"],
-                   ["name": "ガンジー", "image": "gandhi", "proverb": "速度を上げるばかりが、人生ではない。"],
+                   ["name": "ガンジー", "image": "gandhi", "proverb": "他人に変わって欲しければ、自ら率先して変化の原動力となるべきだ。"],
                    ["name": "アインシュタイン", "image": "einstein", "proverb": "天才とは努力する凡才のことである。"],
-                   ["name": "ヘレン・ケラー", "image": "helen_keller", "proverb": "人生はどちらかです。勇気をもって挑むか、棒にふるか。"]]
+                   ["name": "ヘレン・ケラー", "image": "helen_keller", "proverb": "人生はどちらかです。勇気をもって挑むか、棒にふるか。"],
+                   ["name": "ヒポクラテス", "image": "nigaoe_hippocrates.png", "proverb": "心に起きることは全て身体に影響し、身体に起きることもまた心に影響する。"],
+                   ["name": "ソクラテス", "image": "nigaoe_socrates.png", "proverb": "よりよく生きる道を探し続けることが、最高の人生を生きることだ。"],
+                   ["name": "ユング", "image": "nigaoe_jung_yungu.png", "proverb": "他人に対して感じるいらだち”や”不快感”は、自分がどんな人間なのかを教えてくれる。"],
+                   ["name": "ゲーテ", "image": "nigaoe_goethe.png", "proverb": "自分自身を信じてみるだけでいい。きっと、生きる道が見えてくる。"],
+                   ["name": "シェイクスピア", "image": "nigaoe_shakespeare.png", "proverb": "天は自ら行動しない者に救いの手をさしのべない。"],
+                   ["name": "モーツアルト", "image": "mozart.png", "proverb": "多くのことをなす近道は、一度にひとつのことだけすることだ"],
+                   ["name": "孫武", "image": "nigaoe_sonbu.png", "proverb": "主は怒りを以て師を興すべからず、将はいきどおりを以て戦いを致すべからず。"]
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
